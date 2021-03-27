8a6f8ff91119f86c8637bdc037d7dd92d10496bb
Felipe Gregorio <f.gregorio@outlook.com>
2021-03-26 21:08:39 -0300

    Added classes: Board, Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,11 @@
 package application;
 
-import boardgame.Position;
+import boardgame.Board;
 
 public class Program {
 
     public static void main(String[] args) {
 
-        Position myPos = new Position(6, 7);
-        System.out.println(myPos);
+        Board board = new Board(8, 8);
     }
 }
